9a30f39d3410247bde322482b63ca673f5d1db28
Max Yurlov <maxyurloff@gmail.com>
2010-10-04 19:01:07 +0300

    ticket 3 for #4: tests

--- basic/base/tests.py
+++ basic/base/tests.py
@@ -1,5 +1,9 @@
 from django.test import Client, TestCase
 from base.models import Location
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.conf import settings
+
 
 class SimpleTest(TestCase):
     fixtures = ['base.json']
@@ -16,5 +20,10 @@ class SimpleTest(TestCase):
         response = self.client.get('/')
         self.failUnlessEqual(count+1, Location.objects.count())    
 
-    
+    def test_settings(self):
+        response = self.client.get('/settings')
+        self.failUnlessEqual(response.status_code, 200)            
+        self.failUnlessEqual(response.content,
+            loader.get_template('settings.html').render(Context(
+                {'settings': settings,})))
 
