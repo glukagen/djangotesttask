70ad08ba36a5b31f738337c2f973b268bef89f58
Max Yurlov <maxyurloff@gmail.com>
2010-10-04 17:27:08 +0300

    ticket 1 for #1

--- basic/base/views.py
+++ basic/base/views.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from django.shortcuts import get_object_or_404, render_to_response
+from base.models import Person
+
 def firstpage(request):
-    pass
+    return render_to_response('index.html',
+        {'object': get_object_or_404(Person, pk=1)})
+
+
+
