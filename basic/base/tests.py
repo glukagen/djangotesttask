13363d06c9f9629c0ab1379b69e88b74277102da
Max Yurlov <maxyurloff@gmail.com>
2010-10-04 19:24:54 +0300

    ticket 5 for #2: tests

--- basic/base/tests.py
+++ basic/base/tests.py
@@ -3,13 +3,19 @@ from base.models import Location, Person
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class SimpleTest(TestCase):
     fixtures = ['base.json']
-
+    password = 'password'
+    username = 'name'
+    
     def setUp(self):
         self.client = Client()
-
+        self.user = User.objects.create_user(self.username, 'tt@tt.com', self.password)        
+        self.user.is_staff = True
+        self.user.save()
+        
     def test_firstpage(self):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 200)
@@ -41,5 +47,16 @@ class SimpleTest(TestCase):
         self.failUnlessEqual(data['name'], p.name)
         self.failUnlessEqual(data['surname'], p.surname)
         self.failUnlessEqual(data['bio'], p.bio)
-        self.failUnlessEqual(data['contacts'], p.contacts)    
+        self.failUnlessEqual(data['contacts'], p.contacts)
+        
+    def test_login(self):
+        response = self.client.get('/login')
+        self.failUnlessEqual(response.status_code, 200)
+        
+        response = self.client.post('/login', {'username': self.username, 'password' : self.password})
+        self.assertRedirects(response, '/')
+        
+        self.login()
+        response = self.client.get('/login')
+        self.assertRedirects(response, '/')     
 
