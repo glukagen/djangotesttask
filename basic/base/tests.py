77afb48733d17d637ca21f685534b5b6d5f0391c
Max Yurlov <maxyurloff@gmail.com>
2010-10-04 19:10:51 +0300

    ticket 4 for #5: tests

--- basic/base/tests.py
+++ basic/base/tests.py
@@ -1,9 +1,8 @@
 from django.test import Client, TestCase
-from base.models import Location
+from base.models import Location, Person
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.conf import settings
-
 
 class SimpleTest(TestCase):
     fixtures = ['base.json']
@@ -26,4 +25,21 @@ class SimpleTest(TestCase):
         self.failUnlessEqual(response.content,
             loader.get_template('settings.html').render(Context(
                 {'settings': settings,})))
+        
+    def test_form_person_save(self):
+        data = {
+            'name': 'test_name',
+            'surname' : 'test_surname',
+            'bio' : 'test_bio',
+            'contacts' : 'test_contacts'
+        }
+        
+        response = self.client.post('/', data)
+        self.failUnlessEqual(response.status_code, 200)
+        
+        p = Person.objects.get(pk=1)
+        self.failUnlessEqual(data['name'], p.name)
+        self.failUnlessEqual(data['surname'], p.surname)
+        self.failUnlessEqual(data['bio'], p.bio)
+        self.failUnlessEqual(data['contacts'], p.contacts)    
 
