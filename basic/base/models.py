9fab99038e202bc9b6eb558bee1860f43ccd4dcf
Max Yurlov <maxyurloff@gmail.com>
2010-10-05 00:44:30 +0300

    ticket 13 for #13

--- basic/base/models.py
+++ basic/base/models.py
@@ -16,7 +16,11 @@ class Person(models.Model):
         
 class Location(models.Model):
     name = models.CharField(max_length=255)
-
+    PRIORITY_CHOICES = (
+        ('0', '1-choice'),
+        ('l', '2-choice'),
+    )
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
     def __unicode__(self):
         return self.name
 
