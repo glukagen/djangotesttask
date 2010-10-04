4f95f9e6e93092ce85878f11b3fb524b0f9558d0
Max Yurlov <maxyurloff@gmail.com>
2010-10-04 18:44:23 +0300

    ticket 2 for #3

---
+++ basic/base/middleware.py
@@ -0,0 +1,8 @@
+from base.models import Location
+
+class MyMiddleware(object):
+    def process_request(self, request):
+        loc = Location(name=request.get_full_path())
+        loc.save()
+
+
