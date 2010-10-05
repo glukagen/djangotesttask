6e7508a762e4acd706a51006ef17304deb1e9f0b
Max Yurlov <maxyurloff@gmail.com>
2010-10-05 20:19:26 +0300

    add jquery.action[press enter]

--- basic/media/main.js
+++ basic/media/main.js
@@ -1,18 +1,19 @@
-main = {  
-    run: function(){
-        $('#save_button').click(function(){
-            $('#person_form').ajaxSubmit({dataType : 'json', success: function(o) {
-                $('.error').empty();
-                if(o === 1)
-                    $('#form_state').html('Person was successfully updated');
-                else
-                    if(typeof(o.errors) != undefined)
-                        for(i in o.errors)
-                            $('#' + i + '_error').html(i + ':: ' +o.errors[i][0]);    
-            }}); 
-        });
-    }
-}
-$(document).ready(function(){
-    main.run();
+$(function(){
+    $('#save_button').click(function(){
+        $('#person_form').ajaxSubmit({dataType : 'json', success: function(o) {
+            $('.error').empty();
+            if(o === 1)
+                $('#form_state').html('Person was successfully updated');
+            else
+                if(typeof(o.errors) != undefined)
+                    for(i in o.errors)
+                        $('#' + i + '_error').html(i + ':: ' +o.errors[i][0]);    
+        }}); 
+    });
+    
+    $('#person_form').keypress(function(e) {
+        if(e.keyCode == 13)
+            $('#save_button').trigger('click');
+    });
+
 });
